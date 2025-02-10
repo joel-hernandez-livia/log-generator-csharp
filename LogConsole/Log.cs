beebf4627089cc7843699e02aeb90f65374c9bf8
HernandezLivia <joel.sw26@gmail.com>
2025-02-09 19:14:47 -0500

    commit version 1

---
+++ LogConsole/Log.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogConsole
+{
+    internal class Log
+    {
+        private string path = "";
+        //private string salto = "\n";
+
+        public Log(string path)
+        {
+            this.path = path;
+        }
+
+        public void Add(string sLog)
+        {
+            CreateDirectory();
+            string nameFile = GetNameFile();
+            string cadena = "";
+            cadena += $"{DateTime.Now} {sLog} {Environment.NewLine}";
+
+            StreamWriter sw = new StreamWriter(path+"/"+nameFile, true);
+            sw.Write(cadena);
+            sw.Close();
+        }
+
+        private string GetNameFile()
+        {
+            string nameFile="";
+            nameFile = $"log_{DateTime.Now.Day}_{DateTime.Now.Month}_{DateTime.Now.Year}.txt";
+            return nameFile;
+        }
+        public void CreateDirectory() {
+            try
+            {
+                if (!Directory.Exists(path)) 
+                { 
+                    Directory.CreateDirectory(path);
+                }
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ LogConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LogConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Un programa\n");
+            //Log log = new Log(@"C:\Users\JOEL\source\repos\LogConsoleSol\LogConsole\Log");
+            //string relativePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log");
+            string relativePath = @".\Log";
+            Log log = new Log(relativePath);
+            log.Add("Prueba 2");
+
+            Console.WriteLine("\nPresiona cualquier tecla...");
+            Console.ReadKey();
+        }
+    }
+}
